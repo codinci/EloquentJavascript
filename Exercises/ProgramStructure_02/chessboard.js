360a90f51f0d664aafee8aefcb694c45dbbb9ae4
codinci <dkibau@gmail.com>
2022-06-05 23:36:46 +0300

    Refractored program structure

--- Exercises/ProgramStructure_02/chessboard.js
+++ Exercises/ProgramStructure_02/chessboard.js
@@ -1,7 +1,7 @@
-let size = 8;
+let size = 12;
 let board = '';
-for (let i =0; i<= size; i++){
-    for(let j = 0; j<= size; j++){
+for (let i =0; i< size; i++){
+    for(let j = 0; j< size; j++){
        if((i+j) % 2 == 0 ){
            board += ' ';
        } else{
